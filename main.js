7b61f6bad511ed93b7a70c3f0593e64df244f9da
raklord <shaehome19@hotmail.co.uk>
2025-12-17 19:27:53 +0000

    updates n stuff

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-// main.js
-import { initGame } from './gameLoop.js';
-initGame();
+
+import { D } from './numbers.js'
+import { load, save } from './save.js'
+import { newPlayer } from './state.js'
+import { LAYERS } from './defs.js'
+
+let player = load() ?? newPlayer(LAYERS)
+
+window.player = player
+window.D = D
+window.game = { save: () => save(player) }
+
+window.player = player;
+window.game = { save, load, hardReset, tickOnce, setActiveLayer };
