7947d01a94106f5ff5050d8458cbd7509ceb27a2
raklord <shaehome19@hotmail.co.uk>
2025-12-17 19:06:23 +0000

    setup

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: 'https://RakLord.github.io/Unsure/',
-})
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/Unsure/' : '/',
+}))
